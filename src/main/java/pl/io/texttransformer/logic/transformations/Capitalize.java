78d610c4da40a22850dd0da8134c9dfa9e81bf78
dddddddddtd <kozjach@gmail.com>
2019-11-23 15:18:30 +0100

    Add transformations LowerCase, Capitalize and Inverse.

---
+++ src/main/java/pl/io/texttransformer/logic/transformations/Capitalize.java
@@ -0,0 +1,23 @@
+package pl.io.texttransformer.logic.transformations;
+
+import org.springframework.util.StringUtils;
+import pl.io.texttransformer.logic.Transformation;
+
+public class Capitalize extends Transformation {
+
+    private Transformation transformation;
+
+    public Capitalize(Transformation transformation) {
+        this.transformation = transformation;
+    }
+
+    @Override
+    public String transform(String text) {
+        String[] textsplit = text.split(" ");
+        for(int i=0; i<textsplit.length; i++){
+            textsplit[i]=StringUtils.capitalize(textsplit[i]);
+        }
+
+        return String.join(" ", textsplit);
+    }
+}
---
+++ src/main/java/pl/io/texttransformer/logic/transformations/Inverse.java
@@ -0,0 +1,22 @@
+package pl.io.texttransformer.logic.transformations;
+
+import pl.io.texttransformer.logic.Transformation;
+
+public class Inverse extends Transformation {
+
+    private Transformation transformation;
+
+    public Inverse(Transformation transformation) {
+        this.transformation = transformation;
+    }
+
+    @Override
+    public String transform(String text) {
+        byte [] strAsByteArray = text.getBytes();
+        byte [] result = new byte [strAsByteArray.length];
+
+        for (int i = 0; i<strAsByteArray.length; i++)
+            result[i] = strAsByteArray[strAsByteArray.length-i-1];
+        return new String(result);
+    }
+}
