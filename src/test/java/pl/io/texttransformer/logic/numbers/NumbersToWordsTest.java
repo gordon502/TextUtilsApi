64b9e86655ce8922ac9998da93d4e3f3ae620ee0
gordon502 <gordonarek@gmail.com>
2019-11-28 16:59:15 +0100

    Add NumbersToWords Test

---
+++ src/test/java/pl/io/texttransformer/logic/numbers/NumbersToWordsTest.java
@@ -0,0 +1,19 @@
+package pl.io.texttransformer.logic.numbers;
+
+import org.junit.Test;
+import pl.io.texttransformer.logic.Transformation;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumbersToWordsTest {
+
+    @Test
+    public void transform() {
+        NumbersToWords t = new NumbersToWords(new Transformation());
+
+        assertEquals(t.transform("elo byku 123"), "elo byku sto dwadziescia trzy");
+        assertEquals(t.transform(""), "");
+        assertEquals(t.transform("WitaM PaNa 123, 10."), "WitaM PaNa sto dwadziescia trzy, dziesiec.");
+        assertEquals(t.transform("Czesc 12,3543 120"), "Czesc 12,3543 sto dwadziescia");
+    }
+}
