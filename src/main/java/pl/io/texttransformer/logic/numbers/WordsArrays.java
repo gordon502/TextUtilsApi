d2547d734b09e7753daa15e598e686d8b158978c
gordon502 <gordonarek@gmail.com>
2019-12-01 15:50:57 +0100

    Create WordsArray class. Move arrays from NumbersToWords SingleNumberTransform method to WordsArray class.

---
+++ src/main/java/pl/io/texttransformer/logic/numbers/WordsArrays.java
@@ -0,0 +1,27 @@
+package pl.io.texttransformer.logic.numbers;
+
+public class WordsArrays {
+    public static String[] onesArray = { "", "jeden ", "dwa ", "trzy ", "cztery ",
+            "piec ", "szesc ", "siedem ", "osiem ", "dziewiec ", };
+
+    public static String[] tensArray = { "", "jedenascie ", "dwanascie ", "trzynascie ",
+            "czternascie ", "pietnascie ", "szesnascie ", "siedemnascie ",
+            "osiemnascie ", "dziewietnascie ", };
+
+    public static String[] fulltensArray = { "", "dziesiec ", "dwadziescia ",
+            "trzydziesci ", "czterdziesci ", "piecdziesiat ",
+            "szescdziesiat ", "siedemdziesiat ", "osiemdziesiat ",
+            "dziewiecdziesiat ", };
+
+    public static String[] hundredsArray = { "", "sto ", "dwiescie ", "trzysta ", "czterysta ",
+            "piecset ", "szescset ", "siedemset ", "osiemset ",
+            "dziewiecset ", };
+
+    public static String[][] groupsArray = { { "", "", "" },
+            { "tysiac ", "tysiace ", "tysiecy " },
+            { "milion ", "miliony ", "milionow " },
+            { "miliard ", "miliardy ", "miliardow " },
+            { "bilion ", "biliony ", "bilionow " },
+            { "biliard ", "biliardy ", "biliardow " },
+            { "trylion ", "tryliony ", "trylionow " }, };
+}
---
+++ src/main/java/pl/io/texttransformer/logic/numbers/WordsToNumber.java
@@ -0,0 +1,16 @@
+package pl.io.texttransformer.logic.numbers;
+
+import pl.io.texttransformer.logic.Transformation;
+
+public class WordsToNumber extends Transformation {
+
+    private Transformation transformation;
+
+    public WordsToNumber(Transformation transformation) { this.transformation = transformation; }
+
+    @Override
+    public String transform(String text){
+
+    }
+
+}
