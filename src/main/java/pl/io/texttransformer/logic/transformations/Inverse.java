fa20bd22550fbc65ff080672ba963b45c45139f6
dddddddddtd <kozjach@gmail.com>
2019-12-02 13:34:48 +0100

    Add comments to transformations.

--- src/main/java/pl/io/texttransformer/logic/transformations/Inverse.java
+++ src/main/java/pl/io/texttransformer/logic/transformations/Inverse.java
@@ -2,14 +2,26 @@ package pl.io.texttransformer.logic.transformations;
 
 import pl.io.texttransformer.logic.Transformation;
 
+/**
+ * Inverts input text.
+ */
 public class Inverse extends Transformation {
 
     private Transformation transformation;
 
+    /**
+     * Class constructor.
+     * @param transformation Previous transformation.
+     */
     public Inverse(Transformation transformation) {
         this.transformation = transformation;
     }
 
+    /**
+     * Converts String into array of bytes and inverts the order of characters.
+     * @param text Input text modified by previous transformations.
+     * @return String Text modified by transformation.
+     */
     @Override
     public String transform(String text) {
         byte [] strAsByteArray = text.getBytes();
--- src/main/java/pl/io/texttransformer/logic/transformations/UpperCase.java
+++ src/main/java/pl/io/texttransformer/logic/transformations/UpperCase.java
@@ -2,14 +2,26 @@ package pl.io.texttransformer.logic.transformations;
 
 import pl.io.texttransformer.logic.Transformation;
 
+/**
+ * Changes all letters to uppercase.
+ */
 public class UpperCase extends Transformation {
 
     private Transformation transformation;
 
+    /**
+     * Class constructor.
+     * @param transformation Previous transformation.
+     */
     public UpperCase(Transformation transformation) {
         this.transformation = transformation;
     }
 
+    /**
+     * Changes all letters to uppercase using standard Java method String toUpperCase().
+     * @param text Input text modified by previous transformations.
+     * @return String Text modified by transformation.
+     */
     @Override
     public String transform(String text) {
         return transformation.transform(text.toUpperCase());
